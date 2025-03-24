17e21149ec75a4329b7b5441446ed69a232399cf
Nicollas Diniz <101371415+Dinizim@users.noreply.github.com>
2025-03-24 05:03:47 -0300

    Add(Run from Game.py)

--- code/game.py
+++ code/game.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from code.menu import Menu
+
+W_WIDTH = 1240
+W_HEIGHT = 720
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window: Surface = pygame.display.set_mode(size=(W_WIDTH, W_HEIGHT))
 
-    def run(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.Run()
+            pass
+
+
+
+        # running = True
+        # while running:
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT:  
+        #             running = False
+        # pygame.quit()
+
+
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import pygame
-from pygame import Surface, Rect
+from code.game import Game
 
-W_WIDTH = 1240
-W_HEIGHT = 720
 
-pygame.init()
-
-window: Surface = pygame.display.set_mode(size=(W_WIDTH, W_HEIGHT))
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  
-            running = False
-
-pygame.quit()
+game = Game()
+game.run()
